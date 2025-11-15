2a76d482414f3d96dd67dc2dce621b224270cc62
Lemonka <animasoon@gmail.com>
2025-11-15 17:39:14 +0500

    Added sounds and effects

--- Assets/Course Library/Script/PlayerController.cs
+++ Assets/Course Library/Script/PlayerController.cs
@@ -10,30 +10,55 @@ public class PlayerController : MonoBehaviour
     public bool isGameOver = false;
     private bool isOnGround = true;
 
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    private AudioSource playerAudio;
+
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         playerAnim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
 
         Physics.gravity *= gravityModifier;
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     public void OnJump()
     {
-        if (isOnGround)
+        if (isOnGround && !isGameOver)
         {
+            isOnGround = false;
+
             playerAnim.SetTrigger("Jump_trig");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
+
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
+        {
             isOnGround = true;
+
+            dirtParticle.Play();
+        }
         else if (collision.gameObject.CompareTag("Obstacle"))
+        {
             isGameOver = true;
+
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
     }
 }
