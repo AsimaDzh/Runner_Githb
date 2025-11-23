054a7b12c9d916e2e68abee432a5445611e16125
Lemonka <animasoon@gmail.com>
2025-11-23 18:30:35 +0500

    Added spawn enemy and shooting

--- Assets/Course Library/Script/EnemySpawn.cs
+++ Assets/Course Library/Script/EnemySpawn.cs
@@ -3,30 +3,56 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     private Object _enemyRef;
-    private GameObject _visualEnemy;
+    private Vector3 _startPosition;
+    [SerializeField] private float delayBeforeDestroy = 5f;
+
+
+    private PlayerController playerControllerScript;
+
+    public GameObject enemyShootPrefab;
+    public Transform enemyShootPoint;
+    [SerializeField] private float shootSpeed = 20f;
+
 
     void Start()
     {
+        playerControllerScript = GameObject.Find("Woman").GetComponent<PlayerController>();
+        _startPosition = transform.position;
         _enemyRef = Resources.Load("waspy");
-        _visualEnemy = transform.GetChild(0).gameObject;
+
+        InvokeRepeating("EnemyShoot", 2f, 2f);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
-            _visualEnemy.SetActive(false);
-            Invoke("Respawn", 5f);
+            gameObject.SetActive(false);
+            Invoke("Respawn", delayBeforeDestroy);
             Destroy(other.gameObject);
         }
     }
 
     private void Respawn()
     {
-        GameObject enemyClone = (GameObject)Instantiate(_enemyRef);
-        enemyClone.transform.position = transform.position;
-
+        if (!playerControllerScript.isGameOver)
+        {
+            GameObject enemyClone = (GameObject)Instantiate(_enemyRef);
+            enemyClone.transform.position = new Vector3(Random.Range(
+                _startPosition.x - 2, _startPosition.x + 2),
+                _startPosition.y,
+                _startPosition.z);
+        }
         Destroy(gameObject);
     }
-}
 
+    void EnemyShoot()
+    {
+        if (!playerControllerScript.isGameOver)
+        {
+            var bullet = Instantiate(enemyShootPrefab, enemyShootPoint.position, Quaternion.identity);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.linearVelocity = Vector3.left * shootSpeed;
+        }
+    }
+}
