5b9e7eefdb5db3ecfd80ce1ce243138a0325a9eb
AsimaDzh <animasoon@gmail.com>
2025-11-26 00:36:53 +0600

    Added death animation, effects, sound to enemy

--- Assets/Course Library/Script/EnemySpawn.cs
+++ Assets/Course Library/Script/EnemySpawn.cs
@@ -1,36 +1,56 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
     private Object _enemyRef;
     private Vector3 _startPosition;
-    [SerializeField] private float delayBeforeDestroy = 5f;
-
+    private float delayBeforeDestroy = 5f;
 
     private PlayerController playerControllerScript;
+    private Animator enemyAnim;
 
-    public GameObject enemyShootPrefab;
-    public Transform enemyShootPoint;
-    [SerializeField] private float shootSpeed = 20f;
+    public bool isDeadFlag = false;
+    public ParticleSystem deathParticle;
 
+    public AudioClip deathSound;
+    private AudioSource enemyAudio;
 
     void Start()
     {
         playerControllerScript = GameObject.Find("Woman").GetComponent<PlayerController>();
-        _startPosition = transform.position;
         _enemyRef = Resources.Load("waspy");
+        enemyAnim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
 
-        InvokeRepeating("EnemyShoot", 2f, 2f);
+        _startPosition = transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Bullet"))
+        if (other.CompareTag("Bullet") && !isDeadFlag)
         {
-            gameObject.SetActive(false);
-            Invoke("Respawn", delayBeforeDestroy);
+            isDeadFlag = true;
             Destroy(other.gameObject);
+
+            enemyAudio.PlayOneShot(deathSound, 1.0f);
+            deathParticle.Play();
+            enemyAnim.SetBool("isDead", true);
+
+            var col = GetComponent<Collider>();
+            if (col != null) col.enabled = false;
+
+            StartCoroutine(DeathAndRespawnRoutine());
         }
+    }
+
+    private IEnumerator DeathAndRespawnRoutine()
+    {
+        float waitTime = 0.8f;
+        yield return new WaitForSeconds(waitTime);
+
+        gameObject.SetActive(false);
+        Invoke("Respawn", delayBeforeDestroy);
     }
 
     private void Respawn()
@@ -39,20 +59,10 @@ public class EnemySpawn : MonoBehaviour
         {
             GameObject enemyClone = (GameObject)Instantiate(_enemyRef);
             enemyClone.transform.position = new Vector3(Random.Range(
-                _startPosition.x - 2, _startPosition.x + 2),
+                _startPosition.x - 1, _startPosition.x + 1),
                 _startPosition.y,
                 _startPosition.z);
         }
         Destroy(gameObject);
-    }
-
-    void EnemyShoot()
-    {
-        if (!playerControllerScript.isGameOver)
-        {
-            var bullet = Instantiate(enemyShootPrefab, enemyShootPoint.position, Quaternion.identity);
-            Rigidbody rb = bullet.GetComponent<Rigidbody>();
-            rb.linearVelocity = Vector3.left * shootSpeed;
-        }
     }
 }
--- Assets/Course Library/Script/ShootSpawn.cs
+++ Assets/Course Library/Script/ShootSpawn.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class ShootSpawn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject enemyShootPrefab;
+    public Transform enemyShootPoint;
+    [SerializeField] private float shootSpeed = 20f;
+    [SerializeField] private float timer;
+
+    private PlayerController playerControllerScript;
+    private EnemySpawn enemySpawnScript;
+
+
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Woman").GetComponent<PlayerController>();
+        enemySpawnScript = GetComponentInParent<EnemySpawn>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer > 2f)
+        {
+            timer = 0f;
+            EnemyShoot();
+        }
+    }
+
+    void EnemyShoot()
+    {
+        if (!playerControllerScript.isGameOver && !enemySpawnScript.isDeadFlag)
+        {
+            var bullet = Instantiate(enemyShootPrefab, enemyShootPoint.position, Quaternion.identity);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.linearVelocity = Vector3.left * shootSpeed;
+        }
     }
 }
