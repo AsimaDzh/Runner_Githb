641f0536d21d37aafdb01a333427949c9d961aa0
AsimaDzh <animasoon@gmail.com>
2025-12-07 16:29:59 +0600

    Added obsticle

--- Assets/Course Library/Script/CollectFiles.cs
+++ Assets/Course Library/Script/CollectFiles.cs
@@ -6,6 +6,7 @@ public class CollectFiles : MonoBehaviour
     private int fileCount = 0;
 
     public TextMeshProUGUI fileCountText;
+    public TextMeshProUGUI gameOverFilesText;
 
     void OnTriggerEnter(Collider other)
     {
@@ -13,6 +14,7 @@ public class CollectFiles : MonoBehaviour
         {
             fileCount++;
             fileCountText.text = fileCount.ToString();
+            gameOverFilesText.text = "You ate " + fileCount.ToString() + " files";
             Destroy(other.gameObject);
         }
     }
